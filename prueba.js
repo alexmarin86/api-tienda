6a0e7fb5885ad7264d823b9cd006e580ee549bba
alexmarin86 <alexeu86@gmail.com>
2022-06-30 08:06:59 +0200

    archivo no necesario eliminado

--- prueba.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-	const btnAtras = document.querySelectorAll('.volver-atras')
-	btnAtras.forEach((element) => {
-		element.addEventListener('click', (e) => {
-			e.preventDefault()
-			window.history.back()
-		})
-	})
-})
